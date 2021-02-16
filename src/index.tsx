0dfa7bff4c8c6811868d7c4a4719d7343e70b021
Dan <qqwwee08@naver.com>
2021-02-17 00:01:08 +0900

    redux 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
 import themes from "./styles/themes";
 import { ThemeProvider } from "./styles/themed-components";
 import GlobalStyle from "./styles/global-styles";
-import configureStore from "./createStore";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
-const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.Fragment>
     <GlobalStyle />
     <ThemeProvider theme={themes}>
       <App />
     </ThemeProvider>
-  </Provider>,
+  </React.Fragment>,
   document.getElementById("root")
 );
